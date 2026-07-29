d7f8658887590fc762a2a34ff77bec48e230e41c
Ahmed02001 <ahmedhamdyy787@gmail.com>
2026-07-29 22:45:33 +0300

    feat: implement JWT authentication with register, login, and auth middleware

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
+import authenticate from "./middlewares/auth.middleware.js";
 
 export const app = express();
 
@@ -13,3 +14,7 @@ app.get("/health", (req, res) =>
 );
 
 app.use("/api/auth", authRouter);
+
+app.get("/api/test-auth", authenticate, (req, res) => {
+  res.json({ message: "You are authenticated!", user: req.user });
+});
