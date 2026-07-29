2a107d24c1da347617df672117b800d87bc31550
Ahmed02001 <ahmedhamdyy787@gmail.com>
2026-07-29 15:46:58 +0300

    feat: add PostgreSQL database with Prisma ORM and Docker

--- Backend/src/config/env.js
+++ Backend/src/config/env.js
@@ -4,6 +4,9 @@ dotenv.config({ quiet: true });
 
 const ENV = {
   PORT: process.env.PORT || 3000,
+  DATABASE_URL:
+    process.env.DATABASE_URL ||
+    "postgresql://postgres:fsafkjhre3469873ggfbrt@localhost:5433/testManagementDb?schema=public",
 };
 
 export default ENV;
